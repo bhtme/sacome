535b7e277823e3d766b7a2ff30c3db4a90436b41
tuliorcc <>
2018-04-12 11:36:19 -0300

    Criados Value Objects

---
+++ src/java/sacome/beans/Consulta.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sacome.beans;
+
+/**
+ *
+ * @author tulio
+ */
+public class Consulta {
+    private int id;
+    private String cpf, crm;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+    
+    
+}
