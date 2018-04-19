f74abf37393005ba3199c83873df907cd48762f3
tuliorcc <>
2018-04-19 10:16:00 -0300

    form bean de consulta

---
+++ src/java/sacome/forms/AddConsultaFormBean.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sacome.forms;
+
+/**
+ *
+ * @author tulio
+ */
+public class AddConsultaFormBean {
+    
+    private String cpf, crm, dataConsulta;
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCrm() {
+        return crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm = crm;
+    }
+
+    public String getDataConsulta() {
+        return dataConsulta;
+    }
+
+    public void setDataConsulta(String dataConsulta) {
+        this.dataConsulta = dataConsulta;
+    }
+    
+    
+}
